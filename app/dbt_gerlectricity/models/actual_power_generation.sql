611eb9d5abc7fe9a9b6a00f8cc5f09ad130eab7b
pahansen <39408669+pahansen@users.noreply.github.com>
2023-04-26 20:07:40 +0000

    fix: date format string

--- app/dbt_gerlectricity/models/actual_power_generation.sql
+++ app/dbt_gerlectricity/models/actual_power_generation.sql
@@ -4,8 +4,8 @@ with raw_actual_power as (
 
 final as (
     select 
-        strptime(concat(Date::varchar, ' ', "Start"), '%Y-%M-%d %H:%M') as start_date,
-        strptime(concat(Date::varchar, ' ', "End"), '%Y-%M-%d %H:%M') as end_date,
+        strptime(concat(Date::varchar, ' ', "Start"), '%Y-%m-%d %H:%M') as start_date,
+        strptime(concat(Date::varchar, ' ', "End"), '%Y-%m-%d %H:%M') as end_date,
         replace(replace("Biomasse [MWh] Calculated resolutions", '.', ''), ',', '.')::double as biomass_MWh,
         replace(replace("Wasserkraft [MWh] Calculated resolutions", '.', ''), ',', '.')::double as waterpower_MWh,
         replace(replace("Wind Offshore [MWh] Calculated resolutions", '.', ''), ',', '.')::double as wind_offshore_MWh,
