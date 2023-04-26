323fa8e2e379d288faea85c9a669331e1b1b1399
pahansen <39408669+pahansen@users.noreply.github.com>
2023-04-26 19:38:06 +0000

    duckdb setup

---
+++ app/db/db_setup.py
@@ -0,0 +1,39 @@
+import duckdb
+
+con = duckdb.connect('/workspaces/gerlectricity/gerlectricity.db')
+con.sql(
+    """
+        create or replace table raw_actual_power_consumption as 
+        select * 
+        from read_csv_auto('/workspaces/gerlectricity/smard_data/Realisierter_Stromverbrauch_202101010000_202303312359_Hour.csv')
+    """
+)
+con.sql(
+    """
+        create or replace table raw_actual_power_generation as 
+        select * 
+        from read_csv_auto('/workspaces/gerlectricity/smard_data/Realisierte_Erzeugung_202101010000_202303312359_Hour.csv')
+    """
+)
+con.sql(
+    """
+        create or replace table raw_predicted_power_consumption as 
+        select * 
+        from read_csv_auto('/workspaces/gerlectricity/smard_data/Prognostizierter_Stromverbrauch_202101010000_202303312359_Stunde.csv')
+    """
+)
+con.sql(
+    """
+        create or replace table raw_predicted_power_generation as 
+        select * 
+        from read_csv_auto('/workspaces/gerlectricity/smard_data/Prognostizierte_Erzeugung_202101010000_202303312359_Hour.csv')
+    """
+)
+con.sql(
+    """
+        create or replace table raw_built_generation_capacity as 
+        select * 
+        from read_csv_auto('/workspaces/gerlectricity/smard_data/Installierte_Erzeugungsleistung_202101010000_202303312359_Year.csv')
+    """
+)
+
---
+++ app/dbt_gerlectricity/models/actual_power_generation.sql
@@ -0,0 +1,24 @@
+with raw_actual_power as (
+    select * from {{ source("main", "raw_actual_power_generation") }}
+),
+
+final as (
+    select 
+        strptime(concat(Date::varchar, ' ', "Start"), '%Y-%M-%d %H:%M') as start_date,
+        strptime(concat(Date::varchar, ' ', "End"), '%Y-%M-%d %H:%M') as end_date,
+        replace(replace("Biomasse [MWh] Calculated resolutions", '.', ''), ',', '.')::double as biomass_MWh,
+        replace(replace("Wasserkraft [MWh] Calculated resolutions", '.', ''), ',', '.')::double as waterpower_MWh,
+        replace(replace("Wind Offshore [MWh] Calculated resolutions", '.', ''), ',', '.')::double as wind_offshore_MWh,
+        replace(replace("Wind Onshore [MWh] Calculated resolutions", '.', ''), ',', '.')::double as wind_onshore_MWh,
+        replace(replace("Photovoltaik [MWh] Calculated resolutions", '.', ''), ',', '.')::double as solar_MWh,
+        replace(replace("Sonstige Erneuerbare [MWh] Calculated resolutions", '.', ''), ',', '.')::double as misc_renewables_MWh,
+        replace(replace("Kernenergie [MWh] Calculated resolutions", '.', ''), ',', '.')::double as nuclear_MWh,
+        replace(replace("Braunkohle [MWh] Calculated resolutions", '.', ''), ',', '.')::double as brown_coal_MWh,
+        replace(replace("Steinkohle [MWh] Calculated resolutions", '.', ''), ',', '.')::double as hard_coal_MWh,
+        replace(replace("Erdgas [MWh] Calculated resolutions", '.', ''), ',', '.')::double as gas_MWh,
+        replace(replace("Pumpspeicher [MWh] Calculated resolutions", '.', ''), ',', '.')::double as pump_storage_MWh,
+        replace(replace("Sonstige Konventionelle [MWh] Calculated resolutions", '.', ''), ',', '.')::double as misc_conventional_MWh,
+    from raw_actual_power_generation
+)
+
+select * from final
